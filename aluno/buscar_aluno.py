53c434217ff5920c40c6703794118c93d07b7063
Armando D. Galiati <armandogaliati@gmail.com>
2025-04-25 16:21:09 -0300

    fix formatacao relatorio alunos

--- aluno/buscar_aluno.py
+++ aluno/buscar_aluno.py
@@ -1,4 +1,6 @@
 def buscar_alunos(alunos):
     with open("inscricoes_aluno.txt", "r") as arquivo:
+
         for linha in arquivo:
-            print(linha)
+            dados = linha.strip().split(",")
+            print(f"Nome: {dados[0]}, Idade: {dados[1]}, Modalidade: {dados[2]}")
